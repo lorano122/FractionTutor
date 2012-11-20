d44b18d2ee1cabc1ec48bcd66552274e675e1baa
Sydney Tomlinson <sydney.tomlinson@gmail.com>
2012-11-19 18:34:12 -0700

    Adding hopefully working files

--- PokemonFractions/gen/com/example/pokemonfractions/R.java
+++ PokemonFractions/gen/com/example/pokemonfractions/R.java
@@ -11,12 +11,30 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int fifth1=0x7f020000;
+        public static final int fifth2=0x7f020001;
+        public static final int fifth3=0x7f020002;
+        public static final int fifth4=0x7f020003;
+        public static final int fifth5=0x7f020004;
+        public static final int fifths_lines=0x7f020005;
+        public static final int gyrados=0x7f020006;
+        public static final int ic_action_search=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int onix=0x7f020009;
+        public static final int pokeball=0x7f02000a;
+        public static final int sixth1=0x7f02000b;
+        public static final int sixth2=0x7f02000c;
+        public static final int sixth3=0x7f02000d;
+        public static final int sixth4=0x7f02000e;
+        public static final int sixth5=0x7f02000f;
+        public static final int sixth6=0x7f020010;
+        public static final int sixths_lines=0x7f020011;
+        public static final int snorlax=0x7f020012;
+        public static final int squirtle=0x7f020013;
     }
     public static final class id {
-        public static final int graphics_holder=0x7f070000;
         public static final int menu_settings=0x7f070001;
+        public static final int pokeball=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;

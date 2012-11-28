7e240d1447ea26a879ad977db93a6a41ba72f690
Christina Thompson <lastofmykind122@gmail.com>
2012-11-27 19:28:01 -0700

    created the MasterAddition activity and gave it a basic skelleton, created the MasterAddition math helper class

--- PokemonFractions/gen/com/example/pokemonfractions/R.java
+++ PokemonFractions/gen/com/example/pokemonfractions/R.java
@@ -57,18 +57,21 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_poke_fling=0x7f030001;
-        public static final int activity_tutorial=0x7f030002;
+        public static final int activity_master_addition=0x7f030001;
+        public static final int activity_poke_fling=0x7f030002;
+        public static final int activity_tutorial=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
-        public static final int activity_poke_fling=0x7f060001;
+        public static final int activity_master_addition=0x7f060001;
+        public static final int activity_poke_fling=0x7f060002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_main=0x7f040003;
+        public static final int title_activity_master_addition=0x7f040006;
         public static final int title_activity_poke_fling=0x7f040004;
         public static final int title_activity_tutorial=0x7f040005;
     }

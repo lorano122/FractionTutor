59e103affd7abbee751f7cb5deb06d83a09d1fc9
Christina Thompson <lastofmykind122@gmail.com>
2012-11-28 17:30:03 -0700

    added an inbetween screen after fling and link the ending of fling to move to this screen

--- PokemonFractions/gen/com/example/pokemonfractions/R.java
+++ PokemonFractions/gen/com/example/pokemonfractions/R.java
@@ -28,46 +28,51 @@ public final class R {
         public static final int fifth4=0x7f02000e;
         public static final int fifth5=0x7f02000f;
         public static final int fifths_lines=0x7f020010;
-        public static final int flingwin=0x7f020011;
-        public static final int gyrados=0x7f020012;
-        public static final int ic_action_search=0x7f020013;
-        public static final int ic_launcher=0x7f020014;
-        public static final int onix=0x7f020015;
-        public static final int pokeball=0x7f020016;
-        public static final int pokedex1=0x7f020017;
-        public static final int scoreball=0x7f020018;
-        public static final int sixth1=0x7f020019;
-        public static final int sixth2=0x7f02001a;
-        public static final int sixth3=0x7f02001b;
-        public static final int sixth4=0x7f02001c;
-        public static final int sixth5=0x7f02001d;
-        public static final int sixth6=0x7f02001e;
-        public static final int sixths_lines=0x7f02001f;
-        public static final int smallball=0x7f020020;
-        public static final int snorlax=0x7f020021;
-        public static final int squirtle=0x7f020022;
+        public static final int gyrados=0x7f020011;
+        public static final int ic_action_search=0x7f020012;
+        public static final int ic_launcher=0x7f020013;
+        public static final int onix=0x7f020014;
+        public static final int pokeball=0x7f020015;
+        public static final int pokedex1=0x7f020016;
+        public static final int scoreball=0x7f020017;
+        public static final int sixth1=0x7f020018;
+        public static final int sixth2=0x7f020019;
+        public static final int sixth3=0x7f02001a;
+        public static final int sixth4=0x7f02001b;
+        public static final int sixth5=0x7f02001c;
+        public static final int sixth6=0x7f02001d;
+        public static final int sixths_lines=0x7f02001e;
+        public static final int smallball=0x7f02001f;
+        public static final int snorlax=0x7f020020;
+        public static final int squirtle=0x7f020021;
     }
     public static final class id {
-        public static final int graphics_holder=0x7f070000;
-        public static final int menu_settings=0x7f070003;
-        public static final int reset=0x7f070001;
-        public static final int start=0x7f070002;
+        public static final int button1=0x7f070001;
+        public static final int graphics_holder=0x7f070002;
+        public static final int menu_settings=0x7f070005;
+        public static final int reset=0x7f070003;
+        public static final int result=0x7f070000;
+        public static final int start=0x7f070004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_master_addition=0x7f030001;
-        public static final int activity_poke_fling=0x7f030002;
-        public static final int activity_tutorial=0x7f030003;
+        public static final int activity_fling_score=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_master_addition=0x7f030002;
+        public static final int activity_poke_fling=0x7f030003;
+        public static final int activity_tutorial=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
-        public static final int activity_master_addition=0x7f060001;
-        public static final int activity_poke_fling=0x7f060002;
+        public static final int activity_fling_score=0x7f060000;
+        public static final int activity_main=0x7f060001;
+        public static final int activity_master_addition=0x7f060002;
+        public static final int activity_poke_fling=0x7f060003;
     }
     public static final class string {
         public static final int Begin=0x7f040001;
         public static final int app_name=0x7f040000;
+        public static final int hello_world=0x7f040007;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_fling_score=0x7f040008;
         public static final int title_activity_main=0x7f040003;
         public static final int title_activity_master_addition=0x7f040006;
         public static final int title_activity_poke_fling=0x7f040004;
